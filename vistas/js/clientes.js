231c18527284633b2b65ab0f5649d9d5a1aa5c89
carlos morales <carlosmoralesl772@gmail.com>
2020-08-27 18:11:30 -0500

    editar informacion del cliente

--- vistas/js/clientes.js
+++ vistas/js/clientes.js
@@ -21,11 +21,7 @@ $(".tablas").on("click", ".btnEditarCliente", function(){
       
       	   $("#idCliente").val(respuesta["id"]);
 	       $("#editarCliente").val(respuesta["nombre"]);
-	       $("#editarDocumentoId").val(respuesta["documento"]);
-	       $("#editarEmail").val(respuesta["email"]);
-	       $("#editarTelefono").val(respuesta["telefono"]);
-	       $("#editarDireccion").val(respuesta["direccion"]);
-           $("#editarFechaNacimiento").val(respuesta["fecha_nacimiento"]);
+ 
 	  }
 
   	})
