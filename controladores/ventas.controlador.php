7ae6396d1f91a32b9dd904fb1406d1c6c800365c
carlos morales <carlosmoralesl772@gmail.com>
2020-08-29 11:39:32 -0500

    editar venta

--- controladores/ventas.controlador.php
+++ controladores/ventas.controlador.php
@@ -119,8 +119,9 @@ class ControladorVentas{
 						   "productos"=>$_POST["listaProductos"],
 						   "impuesto"=>$_POST["nuevoPrecioImpuesto"],
 						   "neto"=>$_POST["nuevoPrecioNeto"],
-						   "total"=>$_POST["totalVenta"],
-						   "metodo_pago"=>$_POST["listaMetodoPago"]);
+						   "total"=>$_POST["totalVenta"]
+						   //"metodo_pago"=>$_POST["listaMetodoPago"]
+						);
 
 			$respuesta = ModeloVentas::mdlIngresarVenta($tabla, $datos);
 
@@ -377,8 +378,9 @@ class ControladorVentas{
 						   "productos"=>$listaProductos,
 						   "impuesto"=>$_POST["nuevoPrecioImpuesto"],
 						   "neto"=>$_POST["nuevoPrecioNeto"],
-						   "total"=>$_POST["totalVenta"],
-						   "metodo_pago"=>$_POST["listaMetodoPago"]);
+						   "total"=>$_POST["totalVenta"]
+						   //"metodo_pago"=>$_POST["listaMetodoPago"]
+						);
 
 
 			$respuesta = ModeloVentas::mdlEditarVenta($tabla, $datos);
