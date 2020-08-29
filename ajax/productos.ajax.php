1bea1f6a6374cb449308f1686988658864c60c90
carlos morales <carlosmoralesl772@gmail.com>
2020-08-29 02:32:25 -0500

    editar productos_error

--- ajax/productos.ajax.php
+++ ajax/productos.ajax.php
@@ -6,8 +6,6 @@ require_once "../modelos/productos.modelo.php";
 require_once "../controladores/categorias.controlador.php";
 require_once "../modelos/categorias.modelo.php";
 
-require_once "../controladores/tipos.controlador.php";
-require_once "../modelos/tipos.modelo.php";
 
 class AjaxProductos{
 
